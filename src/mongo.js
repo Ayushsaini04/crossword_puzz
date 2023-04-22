91decbc647a854f76ba04afedfd9fb8613946769
unknown <ayushsaini643@gmail.com>
2023-04-22 17:12:57 +0530

    first commit

---
+++ src/mongo.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose")
+
+require('dotenv').config();
+
+mongoose.connect('mongodb+srv://ayush:ayush2002@cluster0.kqioxbb.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+});
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
